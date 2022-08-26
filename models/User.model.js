fa80ea2da6636a26f16e394ea4a0a4b29d3ee871
Marko Ivezic <markoivezic89@gmail.com>
2022-08-26 16:40:15 +0200

    user route

--- models/User.model.js
+++ models/User.model.js
@@ -1,26 +1,24 @@
 const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
-const userSchema = new Schema(
-  {
-    username: {
-      type: Schema.Types.String,
-      unique: true
-    },
-    role: {
-      type: Schema.Types.String,
-      enum: ["photographer", "model", "makeup artist"]
-    },
-    password: {
-      type: Schema.Types.String, 
-      required: true
-    },
-    email: {
-      type: Schema.Types.String,
-      required: true,
+const userSchema = new Schema({
+  username: {
+    type: Schema.Types.String,
+    unique: true,
+  },
+  role: {
+    type: Schema.Types.String,
+    enum: ["photographer", "model", "makeup artist"],
+  },
+  password: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  email: {
+    type: Schema.Types.String,
+    required: true,
   },
 });
-
 
 const User = model("User", userSchema);
 

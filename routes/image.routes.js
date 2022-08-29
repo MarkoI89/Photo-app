fca62147b43677487459a8befe32cef9a8508329
Marko Ivezic <markoivezic89@gmail.com>
2022-08-29 16:47:45 +0200

    Added all image routes

--- routes/image.routes.js
+++ routes/image.routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const Image = require("../models/Image.model");
-const User = require("../models/User.model")
+const User = require("../models/User.model");
 // create image
 
 router.post("/", async (req, res, next) => {
@@ -25,8 +25,22 @@ router.post("/", async (req, res, next) => {
 // find image
 router.get("/", async (req, res, next) => {
   try {
-    const allImages = await Image.find();
-    res.status(200).json(allImages);
+    const { shot_by, model: modelUsername, makeup_artist } = req.query;
+    const searchQuery = {};
+    const photographer = await User.findOne({ username: shot_by });
+    const model = await User.findOne({ username: modelUsername });
+    const makeupArtist = await User.findOne({ username: makeup_artist });
+    if (photographer) {
+      searchQuery.shot_by = photographer.id;
+    }
+    if (model) {
+      searchQuery.model = model.id;
+    }
+    if (makeupArtist) {
+      searchQuery.makeup_artist = makeupArtist.id;
+    }
+    const shot_byFilter = await Image.find(searchQuery);
+    res.status(200).json(shot_byFilter);
   } catch (error) {
     next(error);
   }
@@ -35,11 +49,19 @@ router.get("/", async (req, res, next) => {
 // find image by id
 router.get("/:imageId", async (req, res, next) => {
   const { imageId } = req.params;
-  await User.findById(imageId)
+  await Image.findById(imageId)
+    .populate("shot_by model makeup_artist")
     .then((image) => res.status(200).json(image))
     .catch((error) => next(error));
 });
 
 // delete image
+
+router.delete("/:imageId", async (req, res, next) => {
+  const { imageId } = req.params;
+  await Image.findByIdAndDelete(imageId)
+    .then((deletedImage) => res.status(200).json(deletedImage))
+    .catch((error) => next(error));
+});
 
 module.exports = router;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -15,9 +15,14 @@ router.get("/", async (req, res, next) => {
   try {
     // console.log(req.query)
     const { role } = req.query;
-    if (req.query.role) {
+    const {username} = req.query;
+    if (role) {
       const roleFilter = await User.find({ role });
       res.json(roleFilter);
+    }
+    if (username) {
+      const usernameFilter = await User.find({ username });
+      res.json(usernameFilter);
     } else {
       const allUsers = await User.find();
       return res.status(200).json(allUsers);
@@ -61,20 +66,17 @@ router.get("/:userId", async (req, res, next) => {
 
 router.patch("/:userId", async (req, res, next) => {
   const { userId } = req.params;
-  const { username, role, password } = req.body;
-  await User.findByIdAndUpdate(
-    userId,
-    { username, role, password },
-    { new: true }
-  )
+  const { role, password } = req.body;
+  await User.findByIdAndUpdate(userId, { role, password }, { new: true })
     .then((updatedUser) => res.status(200).json(updatedUser))
     .catch((error) => next(error));
 });
+// Delete user
 
 router.delete("/:userId", async (req, res, next) => {
   const { userId } = req.params;
   await User.findByIdAndDelete(userId)
-    .then((deletedUser) => res.status(200).json(deletedUser))
+    .then((deletedUser) => res.sendStatus(204))
     .catch((error) => next(error));
 });
 

705ad4c8edbea59391c84bab3a4bf7417478800f
Marly-Yasuke <marly.diallo2021@gmail.com>
2022-09-15 11:02:19 +0200

    allow few role + more roles added

--- middleware/index.middleware.js
+++ middleware/index.middleware.js
@@ -5,30 +5,6 @@ const User = require("../models/User.model");
 
 
 
-//This middleware will check if the user is a photographer
-const photographerCheck = async (req, res, next) => {
-    //get the role of the user, send its as an object, we can access it from the req.body
-    const user = await User.findOne({
-        username: req.user.username
-    });
-    const role = user ?.role;
-    //check if the role different than photographer, decline 
-    if (role !== "photographer") {
-        //send a json with an error
-        return res.status(401).json({
-            message: 'You need to be a photographer to access'
-        })
-    } else {
-
-        next()
-
-    }
-};
-
-
-
-
-// jwt middleware
 
 const isAuthenticated = async (req, res, next) => {
     try {
@@ -58,6 +34,31 @@ const isAuthenticated = async (req, res, next) => {
     // Once everything went well, go to the next middleware
     next()
 }
+//This middleware will check if the user is a photographer
+const photographerCheck = async (req, res, next) => {
+    await isAuthenticated(req, res, next)
+    return (req, res, next) => {
+
+    const role = req.user.role
+   
+    //check if the role different than photographer, decline 
+    
+    if (role.includes("photographer")) {
+        //send a json with an error
+        return next()
+    } else {
+        return res.status(401).json({
+            message: 'You need to be a photographer to access'
+        })
+        
+
+    }
+};
+
+
+}
+
+// jwt middleware
 
 
 

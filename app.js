de5c1b0038d0831cbf2844e58934b6def238b2a5
Marly-Yasuke <marly.diallo2021@gmail.com>
2022-09-22 17:36:44 +0200

    bug fixing

--- app.js
+++ app.js
@@ -22,12 +22,4 @@ app.use("/api", allRoutes);
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-
-    credentials: true,
-  })
-);
-
 module.exports = app;

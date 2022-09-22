61057ae6a9ede3080f3901ac63ae593a6e930537
Marly-Yasuke <marly.diallo2021@gmail.com>
2022-09-22 16:39:46 +0200

    herroku deployment

--- middleware/index.middleware.js
+++ middleware/index.middleware.js
@@ -1,69 +1,60 @@
 //Create a server
-const jsonWebToken = require('jsonwebtoken');
+const jsonWebToken = require("jsonwebtoken");
 const User = require("../models/User.model");
-
+// client send token on request, then get verified, tjen find user that fits with data in token
 
 const isAuthenticated = async (req, res, next) => {
-    try {
-        let token = req.headers.authorization
-        console.log(req.headers)
-        if (!token) {
-            return res.status(400).json({
-                message: 'No token found!'
-            })
-        }
-        token = token.replace('Bearer ', '')
-        const userToken = jsonWebToken.verify(token, process.env.TOKEN_SECRET)
-        console.log(userToken)
-        const user = await User.findOne({
-            username: userToken.username
-        })
-        if (!user) {
-            return res.status(400).json({
-                message: 'Invalid token'
-            })
-        }
-        req.user = user
-    } catch (error) {
-        return res.status(401).json({
-            message: 'Invalid token'
-        })
+  try {
+    let token = req.headers.authorization;
+    console.log(req.headers);
+    if (!token) {
+      return res.status(400).json({
+        message: "No token found!",
+      });
     }
-    // Once everything went well, go to the next middleware
-    next()
-}
-
-
+    token = token.replace("Bearer ", "");
+    const userToken = jsonWebToken.verify(token, process.env.TOKEN_SECRET);
+    console.log(userToken);
+    const user = await User.findOne({
+      username: userToken.username,
+    });
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid token",
+      });
+    }
+    req.user = user;
+  } catch (error) {
+    return res.status(401).json({
+      message: "Invalid token",
+    });
+  }
+  // Once everything went well, go to the next middleware
+  next();
+};
 
 //This middleware will check if the user is a photographer
 const photographerCheck = async (req, res, next) => {
-    await isAuthenticated(req, res, next)
-    return (req, res, next) => {
+  await isAuthenticated(req, res, next);
+  return (req, res, next) => {
+    const role = req.user.role;
 
-    const role = req.user.role
-   
-    //check if the role different than photographer, decline 
-    
+    //check if the role different than photographer, decline
+
     if (role.includes("photographer")) {
-        //send a json with an error
-        return next()
+      //send a json with an error
+      return next();
     } else {
-        return res.status(401).json({
-            message: 'You need to be a photographer to access'
-        })
-        
-
+      return res.status(401).json({
+        message: "You need to be a photographer to access",
+      });
     }
+  };
 };
-
-
-}
 
 // jwt middleware
 
-
-
 module.exports = {
-    isAuthenticated,
-    photographerCheck,
-}
+  isAuthenticated,
+  photographerCheck,
+};
--- models/Image.model.js
+++ models/Image.model.js
@@ -1,24 +1,22 @@
-const {
-  Schema,
-  model
-} = require("mongoose");
+const { Schema, model } = require("mongoose");
 
 const imageSchema = new Schema({
   link: Schema.Types.String,
   shot_by: {
     type: Schema.Types.ObjectId,
-    ref: 'User',
+    ref: "User",
   },
   model: {
     type: Schema.Types.ObjectId,
-    ref: 'User',
+    ref: "User",
   },
   makeup_artist: {
     type: Schema.Types.ObjectId,
-    ref: 'User',
+    ref: "User",
   },
-}, )
+});
 
+// connexion between models => user
 
 const Image = model("Image", imageSchema);
 

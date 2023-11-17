53080feb0c9b4a1a621ce9c8f1f25f84eec0c344
Marko Ivezic <markoivezic89@gmail.com>
2023-11-17 20:31:49 +0100

    Added new frontend link for cors origin

--- config/index.js
+++ config/index.js
@@ -25,7 +25,7 @@ module.exports = (app) => {
   app.use(
     cors({
       credentials: true,
-      origin: "http://localhost:3000",
+      origin: "https://letshoot.netlify.app/",
     })
   );
 
